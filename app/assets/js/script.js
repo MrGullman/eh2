0f87afb404a49ffb303ae03731350fbd5367838b
Jesper Gullman <jesper.gullman@outlook.com>
2020-01-19 01:38:29 +0100

    Update

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,8 +1,15 @@
 const asideMenu = document.getElementById('asideMenu');
 const menuBtn = document.getElementById('menuBtn');
 const appContainer = document.getElementById('app-container');
+const bar = document.querySelectorAll('.bar');
 
 menuBtn.addEventListener('click', ()=>{
   asideMenu.classList.toggle('open');
   appContainer.classList.toggle('app-stop-scrolling');
+
+  bar.forEach(function(item){
+    item.classList.toggle('close');
+  })
+  // console.log(bar);
+
 })
